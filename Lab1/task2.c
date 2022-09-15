7ef8780eb868e24b8e6422ee83ed444670d996e5
klimbarabanov <magic147147@gmail.com>
2022-09-15 21:43:30 +0300

    add new function and some fix

--- Lab1/task2.c
+++ Lab1/task2.c
@@ -11,35 +11,45 @@ int cloneAtoi(char s[]) {
 //void solveEquation() {
 //
 //}
-//
-//void isMultiple() {
-//
-//}
 
-void isRightTriangle(int a, int b, int c) {
+void isRightTriangle(int a, int b, int c) { //type?
     int aSquared = a*a;
     int bSquared = b*b;
     int cSquared = c*c;
     if(((aSquared+bSquared) == cSquared) || ((aSquared+cSquared) == bSquared) || ((cSquared+bSquared) == aSquared)) {
-        printf("Yes, %d,%d,%d can be sides of a right triangle", a,b,c);
+        printf("Yes, %d,%d,%d can be sides of a right triangle\n", a,b,c);
     } else {
-        printf("No, %d,%d,%d can't be sides of a right triangle", a,b,c);
+        printf("No, %d,%d,%d can't be sides of a right triangle\n", a,b,c);
     }
 }
+
+void isMultiple(float a, float b) { //type?
+    if (((int)a) % ((int)b) == 0) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+}
+
 
 //file flag 1 2 3
 int main(int argc, char * argv[]) {
     if(argc > 5 || argc <4 ){
-        printf("Wrong input");
-        //выход
+        printf("Wrong input!\n");
+        return 0;
     }
+    printf("%d", cloneAtoi(argv[2]));
     if((strcmp(argv[1], "-q") == 0 || strcmp(argv[1], "/q") == 0) && (argc == 5)) {
 //        solveEquation();
     }
     else if((strcmp(argv[1], "-m") == 0 || strcmp(argv[1], "/m") == 0) && (argc ==4)) {
-//        isMultiple();
+        isMultiple(cloneAtoi(argv[2]),cloneAtoi(argv[3]));
     }
     else if((strcmp(argv[1], "-t") == 0 || strcmp(argv[1], "/t") == 0) && (argc ==5))  {
         isRightTriangle(cloneAtoi(argv[2]),cloneAtoi(argv[3]),cloneAtoi(argv[4]));
+    }
+    else {
+        printf("This flag does not exist");
+        return 0;
     }
 }
