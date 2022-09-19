02c409b0e9c20099d49f26f1179c671269ae6215
klimbarabanov <magic147147@gmail.com>
2022-09-19 17:41:10 +0300

    Create new function cloneAtof and bug fixes

--- Lab1/task2.c
+++ Lab1/task2.c
@@ -2,40 +2,49 @@
 #include <string.h>
 #include <math.h>
 #include <limits.h>
+#define EPSILON 1e-7
 
-int cloneAtoi(char *s) {
-    int result = 0, flag = 1,i=0;
-    if(s[0] == '-') {
+float cloneAtof(char *s) {
+    int negative = 1, i = 0, findDot = 0;
+    float result= 0, otstup = 1;
+
+    if (s[0] == '-') {
         i++;
-        flag = -1;
+        negative = -1;
     }
 
-    for(i; s[i] != '\0'; i++) {
-        result = result*10 + s[i] - '0';
+    for (; s[i] != '\0'; i++) {
+        if (findDot) {
+            otstup = otstup / 10;
+            result = result + (s[i] - '0') * otstup;
+        } else {
+            if (s[i] == '.') {
+                findDot = 1;
+            } else {
+                result = result * 10 + (s[i] - '0');
+            }
+        }
     }
-    return result*flag;
-
+    return result * negative;
 }
 
-void findRoots(double a, double b, double c){
-    double d = b*b - 4*a*c;
-    if (d > 0) {
-        double x1 = ((-b)+(sqrt(d)))/(2*a);
-        double x2 = ((-b)-(sqrt(d)))/(2*a);
+void findRoots(float a, float b, float c){
+    float d = b*b - 4*a*c;
+
+    if (fabs(d) > EPSILON) {
+        float x1 = ((-b)+(sqrt(d)))/(2*a);
+        float x2 = ((-b)-(sqrt(d)))/(2*a);
         printf("%fx^2 + %fx + %f: x1=%f x2=%f\n", a,b,c,x1,x2);
     }
-    else if(d == 0) {
-        double x1 = (-b)/(2*a);
+    else if(fabs(d) < EPSILON) {
+        float x1 = (-b)/(2*a);
         printf("%fx^2 + %fx + %f: x=%f\n", a,b,c,x1);
     } else {
-        printf("No roots");
+        printf("No roots\n");
     }
 }
 
-void solveEqation(int n1, int n2, int n3) {
-    double a = n1;
-    double b = n2;
-    double c = n3;
+void solveEqation(float a, float b, float c) {
     double arr[3] = {a,b,c};
     int counter = 1;
 
@@ -53,42 +62,49 @@ void solveEqation(int n1, int n2, int n3) {
 
 }
 
-void isRightTriangle(int a, int b, int c) {
-    unsigned long aSquared = a*a;
-    unsigned long bSquared = b*b;
-    unsigned long cSquared = c*c;
+void isRightTriangle(float a, float b, float c) {
+    float aSquared = a*a;
+    float bSquared = b*b;
+    float cSquared = c*c;
 
     if(((aSquared+bSquared) == cSquared) || ((aSquared+cSquared) == bSquared) || ((cSquared+bSquared) == aSquared)) {
-        printf("Yes, %d,%d,%d can be sides of a right triangle\n", a,b,c);
+        printf("Yes, %f,%f,%f can be sides of a right triangle\n", a,b,c);
     } else {
-        printf("No, %d,%d,%d can't be sides of a right triangle\n", a,b,c);
+        printf("No, %f,%f,%f can't be sides of a right triangle\n", a,b,c);
     }
 }
 
-void isMultiple(int a, int b) {
-    if((a % b) == 0) {
-        printf("Yes\n");
+int isMultiple(float a, float b) {
+    if(fabs(b) < EPSILON) {
+        printf("you wrote 0\n!");
+        return 0;
+    }
+    if(((int)a) % ((int)b) == 0) {
+        return 1;
     } else {
-        printf("No\n");
+        return 0;
     }
 }
 
 //file flag 1 2 3
 int main(int argc, char * argv[]) {
-    if(argc > 5 || argc <4 ){
+    if(argc > 5 || argc < 4 ) {
         printf("Wrong input!\n");
         return 0;
     }
-
     if((strcmp(argv[1], "-q") == 0 || strcmp(argv[1], "/q") == 0) && (argc == 5)) {
-        solveEqation(cloneAtoi(argv[2]),cloneAtoi(argv[3]), cloneAtoi(argv[4]));
+        solveEqation(cloneAtof(argv[2]),cloneAtof(argv[3]), cloneAtof(argv[4]));
     }
     else if((strcmp(argv[1], "-m") == 0 || strcmp(argv[1], "/m") == 0) && (argc ==4)) {
-        isMultiple(cloneAtoi(argv[2]),cloneAtoi(argv[3]));
+        if(isMultiple(cloneAtof(argv[2]),cloneAtof(argv[3]))) {
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
     }
     else if((strcmp(argv[1], "-t") == 0 || strcmp(argv[1], "/t") == 0) && (argc ==5))  {
-        if(cloneAtoi(argv[2]) > 0 && cloneAtoi(argv[3]) > 0 && cloneAtoi(argv[4]) > 0) {
-            isRightTriangle(cloneAtoi(argv[2]),cloneAtoi(argv[3]),cloneAtoi(argv[4]));
+        if(cloneAtof(argv[2]) > 0 && cloneAtof(argv[3]) > 0 && cloneAtof(argv[4]) > 0) {
+            isRightTriangle(cloneAtof(argv[2]),cloneAtof(argv[3]),cloneAtof(argv[4]));
         } else {
             printf("Enter the positive sides of the triangle\n");
         }
