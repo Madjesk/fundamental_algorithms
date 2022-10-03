8647fa9d4a22de5b269e696493d9a2ce3bd18968
klimbarabanov <magic147147@gmail.com>
2022-10-04 00:00:04 +0300

    add checker in function avf_geom, create clone_pow

--- Lab2/task4.c
+++ Lab2/task4.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdarg>
+#include <stdarg.h>
+#include <math.h>
 
-double clone_pow() {
+double clone_pow(double number, int degree) {
+    if(degree==0) {
+        return 1;
+    }
 
+    if(degree > 0) {
+        return number*clone_pow(number, degree-1);
+    } else {
+        return 1.0 / (number*clone_pow(number, (-degree)-1));
+    }
 }
 
 double avg_geom(int count, ...) {
     double res=1.0;
-    va_list args;
+    va_list numbers;
     va_start(numbers, count);
 
     for(int i=0; i< count; i++){
-        res*= v_arg(numbers, double)
+        res*= va_arg(numbers, double);
     }
     va_end(numbers);
-    //добавить проверки
-    return pow(res, 1./count);
+    if((count % 2 == 0) && (res < 0)) {
+        return NAN;
+    }
+    return pow(res, 1.0/count);
 }
 
 int main() {
-    printf("Average geom %lf", avg_geom(2, (double)1, (double)2));
+    double res=avg_geom(2, (double)1, (double)2);
+
+    if(isnan(res)) {
+        printf("NAN");
+        return 0;
+    } else {
+        printf("Average geom= %lf\n", res);
+    }
+
+    printf("2^5= %lf\n", clone_pow(2,5));
+    printf("2^(-5)= %lf\n", clone_pow(2, -5));
 }
