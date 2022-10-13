8e14c68a2f302f2ff171eccb806442e01a9a8c61
klimbarabanov <magic147147@gmail.com>
2022-10-13 11:20:15 +0300

    solved task 8

---
+++ Lab2/task8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+double func(double x) {
+    return x * x - 5 * (sin(x));
+}
+
+double dihot_method(double (*func)(double), double begin, double end, int acc) {
+    double res = 0.0;
+    double epsilon = pow(10, -acc);
+
+    do {
+        res = (begin + end) / 2;
+        if (func(res) * func(begin)<= epsilon) {
+            end = res;
+        }
+        else {
+            begin = res;
+        }
+    }
+    while(fabs(begin-end) >= epsilon);
+
+    return res;
+}
+
+int main() {
+    printf("Answer: %lf\n", dihot_method(func, (double)1.1, (double)3.5, 3));
+}
