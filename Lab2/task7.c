dd48761dc2e230f437655c7f50f47207c321c7a8
klimbarabanov <magic147147@gmail.com>
2022-10-20 13:29:50 +0300

    some fix

--- Lab2/task7.c
+++ Lab2/task7.c
@@ -8,27 +8,29 @@ typedef struct position {
 } position;
 
 int is_convex(int n, ...) {
-    int step = n / 2;
-    position pos[step];
+    int peaks = n / 2;
+    position pos[peaks];
 
     if (n % 2 == 0 && n > 5) {
+        double x1,y1,x2,y2, determinant;
+        int sign, tmp_sign;
         va_list numbers;
         va_start(numbers, n);
-        for (int i = 0; i < step; i++) {
+
+        for (int i = 0; i < peaks; i++) {
             pos[i].x = va_arg(numbers, double);
             pos[i].y = va_arg(numbers, double);
         }
         va_end(numbers);
 
-        int sign, tmp_sign;
-        for (int i = 0; i < step; i++) {
-            int x1 = pos[(i + 1) % step].x - pos[i % step].x;
-            int y1 = pos[(i + 1) % step].y - pos[i % step].x;
+        for (int i = 0; i < peaks; i++) {
+            x1 = pos[(i + 1) % peaks].x - pos[i % peaks].x;
+            y1 = pos[(i + 1) % peaks].y - pos[i % peaks].x;
 
-            int x2 = pos[(i + 2) % step].x - pos[i % step].x;
-            int y2 = pos[(i + 2) % step].y - pos[i % step].x;
+            x2 = pos[(i + 2) % peaks].x - pos[i % peaks].x;
+            y2 = pos[(i + 2) % peaks].y - pos[i % peaks].x;
 
-            int determinant = x1 * y2 - x2 * y1;
+            determinant = x1 * y2 - x2 * y1;
             tmp_sign = determinant > EPSILON ? 1 : 0;
 
             if (i==0)
@@ -56,10 +58,11 @@ double find_value_polinom(double point, int degree, ...) {
 int main() {
     printf("The result: %lf\n", find_value_polinom(2.,3,1.,2.,3.));
 
-    if(is_convex(2,1.,2.) == 1) {
+    int is_convex_ans = is_convex(6 ,1.,1.,2.,2.,3.,3.,4.,4.);
+    if(is_convex_ans == 1) {
         printf("Convex\n");
     }
-    else if(is_convex(2,1.,2.) == 0) {
+    else if(is_convex_ans == 0) {
         printf("Not convex\n");
     }
     else {
