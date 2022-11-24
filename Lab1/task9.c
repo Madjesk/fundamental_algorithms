e4e1c4e86f936dafbb7dd21cc01101593abafa20
klimbarabanov <magic147147@gmail.com>
2022-11-24 11:15:32 +0300

    add realloc

--- Lab1/task9.c
+++ Lab1/task9.c
@@ -6,7 +6,9 @@
 enum {
     SUCCESSFULLY = -3,
     WRONG_INPUT = -4,
-    SMALL_LEN = -52
+    SMALL_LEN = -5,
+    MEMORY_ERROR = -6,
+    WRONG_BASE = -7
 };
 
 int check_number(char *number, int base) {
@@ -44,12 +46,11 @@ int convert_to_10_system(char *str, int base) {
 
 char *reverse(char *str, int len) {
     if (len < 1){
-        return  SMALL_LEN;
+        return  NULL;
     }
     char *reversed_str = (char *)malloc((len + 1)*sizeof(char));
     if(reversed_str == NULL) {
-        printf("Malloc error");
-        return 0;
+        return NULL;
     }
     reversed_str[len] = 0;
 
@@ -59,9 +60,12 @@ char *reverse(char *str, int len) {
     return reversed_str;
 }
 
-char *convert_10_to_other_system(int number, int base) {
-    char reversed_str[50];
+char *convert_10_to_other_system(int number, int base, int size) {
+    char *reversed_str = (char*)malloc(sizeof(char)*size+1); //нужно +1?
     int tmp, i=0;
+    if(base <=1) {
+        return NULL;
+    }
     while(number) {
         tmp = number % base;
         reversed_str[i] = tmp > 9 ? tmp - 10 + 'A' : tmp + '0';
@@ -69,13 +73,52 @@ char *convert_10_to_other_system(int number, int base) {
         i++;
     }
     char * result_str = reverse(reversed_str, i);
-    return result_str;
+    if(result_str != NULL) {
+        return result_str;
+    } else {
+        return NULL;
+    }
+}
+
+int get_str(char** buf, int* size){
+    char c = getchar();
+    char* tmp = NULL;
+    int initial_size = 2, len = 0;
+    *buf = (char*)malloc(initial_size*sizeof(char));
+    if (!*buf){
+        return MEMORY_ERROR;
+    }
+    while(c != EOF && c != '\n' && c != ' '){
+        if (len == initial_size){
+            initial_size*=2; //А точно ли умножение ?
+            tmp = (char*)realloc(*buf, initial_size);
+            if (!tmp){
+                free(*buf);
+                return MEMORY_ERROR;
+            }
+            *buf = tmp;
+        }
+        (*buf)[len++] = c;
+        c = getchar();
+    }
+    if (len == initial_size){
+        initial_size++;
+        tmp = (char*)realloc(*buf, initial_size);
+        if (!tmp){
+            free(*buf);
+            return MEMORY_ERROR;
+        }
+        *buf = tmp;
+    }
+    (*buf)[len] = '\0';
+    *size = len;
+    return SUCCESSFULLY;
 }
 
 int main() {
-    int base, result = 0, max_num = 0;
-    char number[50], max_number[50];
-    char *ptr;
+    int base, max_num = 0, size = 0, result, max_size;
+    char* buff = NULL;
+    char* max_number = NULL;
 
     printf("Enter base: \n");
     scanf("%d", &base);
@@ -84,15 +127,27 @@ int main() {
     }
 
     printf("Enter numbers: \n");
-    while(strcmp(number, "Stop") != 0) {
-        scanf("%s", number);
-        if(strcmp(number, "Stop") != 0) {
-            if(check_number(number, base) == -3) {
-                result = abs(convert_to_10_system(number, base));
+    while(1) {
+        if(get_str(&buff, &size) == -6) {
+            return MEMORY_ERROR;
+        }
+
+        if (!strcmp(buff, "Stop")) {
+            break;
+        } else {
+            if(check_number(buff, base) == -3) {
+                result = abs(convert_to_10_system(buff, base));
                 if(result > max_num) {
+                    free(max_number);
+                    max_number = NULL;
+                    max_number = (char*)malloc(sizeof(char)*size);
+                    max_size = size;
                     max_num = result;
-                    strcpy(max_number, number);
+                    strcpy(max_number, buff);
                 }
+                size = 0;
+                free(buff);
+                buff = NULL;
             } else {
                 return WRONG_INPUT;
             }
@@ -100,8 +155,8 @@ int main() {
     }
 
     printf("Max number modulo: %s\n", max_number);
-    printf("Max number modulo in 9 number system: %s\n", convert_10_to_other_system(max_num, 9));
-    printf("Max number modulo in 18 number system: %s\n", convert_10_to_other_system(max_num, 18));
-    printf("Max number modulo in 27 number system: %s\n", convert_10_to_other_system(max_num, 27));
-    printf("Max number modulo in 36 number system: %s\n", convert_10_to_other_system(max_num, 36));
+    printf("Max number modulo in 9 number system: %s\n", convert_10_to_other_system(max_num, 9, max_size));
+    printf("Max number modulo in 18 number system: %s\n", convert_10_to_other_system(max_num, 18, max_size));
+    printf("Max number modulo in 27 number system: %s\n", convert_10_to_other_system(max_num, 27, max_size));
+    printf("Max number modulo in 36 number system: %s\n", convert_10_to_other_system(max_num, 36, max_size));
 }
