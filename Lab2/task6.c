68fbdf0e927521ec962207661274cee48275481a
klimbarabanov <magic147147@gmail.com>
2022-11-25 11:46:55 +0300

    add search function using alg kmp

--- Lab2/task6.c
+++ Lab2/task6.c
@@ -14,52 +14,49 @@
 
 enum {
     FILE_NOT_FOUND = -1,
-    FOUND = -2
+    SUBSTR_FOUND = -2,
+    SUBSTR_NOT_FOUND = -3,
 };
 
-// int find_str(char *str, char *substr) {
-//
-// }
-int KMP(char *str,char *substr){
-    int i,j,n,m;
-    n = strlen(str);
-    m = strlen(substr);
-    if(m == 0 || *substr == '\0')
-        return -5;
-    if(n < m || n == 0)
-        return -5;
-    int *proc = (int*)malloc(m*sizeof(int));
-    //Prefix-function evaluation
-    *(proc) = 0;
-    for( i = 1,j = 0;i < m;i++){
-        while(j > 0 && *(substr + j) != *(substr + i))
-            j = *(proc + j - 1);
-        if(*(substr + j) == *(substr + i))
+int find_str(char *str, char *substr) {
+    int i, j, str_len, substr_len;
+	str_len = strlen(str);
+	substr_len = strlen(substr);
+	int *prefix = (int*)malloc(substr_len * sizeof(int));
+
+	//Calculate prefix-function
+	prefix[0] = 0;
+	for(i = 1, j = 0; i < substr_len; i++) {
+		while(j > 0 && substr[j] != substr[i])
+			j = prefix[j-1];
+		if(substr[j] == substr[i])
+			j++;
+		prefix[i] = j;
+	}
+
+	//Search substr in str
+	for(i = 0, j = 0; i < str_len; i++){
+		while(j > 0 && substr[j] != str[i])
+			j = prefix[j - 1];
+		if(substr[j] == str[i]) {
             j++;
-        *(proc + i) = j;
-    }
-    //Searching for substr in str
-    for(i = 0,j = 0;i < n;i++){
-        while(j > 0 && *(substr + j) != *(str + i))
-            j=*(proc + j - 1);
-        if(*(substr + j)==*(str + i))
-            j++;
-        if(j == m){
-            free(proc);
-            return FOUND;
-            //position is i-j+1
         }
-    }
-    free(proc);
-    return -100;
+		if(j == substr_len) {
+		    free(prefix);
+            return SUBSTR_FOUND;
+        }
+	}
+    free(prefix);
+	return SUBSTR_NOT_FOUND;
 }
+
 
 int check_files(char *substr, int num_of_files, ...) {
     FILE *fin;
     char *file_name;
     va_list p;
     va_start(p, num_of_files);
-    int isFound = 0, line =1 ;
+    int is_Found = 0, line =1 ;
     char str[BUFSIZ];
     while(num_of_files != 0) {
         line = 1;
@@ -70,9 +67,11 @@ int check_files(char *substr, int num_of_files, ...) {
             return FILE_NOT_FOUND;
         }
         while(fgets(str, sizeof(str), fin) != NULL) {
-            isFound = KMP(str, substr);
-            if(isFound == FOUND) {
-                printf("Substr %s found in file: %s, line=%d\n", substr, file_name, line);
+            is_Found = find_str(str, substr);
+            if(is_Found == SUBSTR_FOUND) {
+                printf("Substr %s found in file: %s, in line=%d\n", substr, file_name, line);
+            } else if(is_Found == SUBSTR_NOT_FOUND) {
+                // printf("Substr %s NOT found in file: %s, in line=%d\n", substr, file_name, line);
             }
             line++;
         }
